d96147269cbee80f060fb7b3a097c70ed148bfcf
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-02-17 21:33:17 +0700

    [ADD] display products

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css'
+
+function Title(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{props.name}</h1>
+  )
 }
 
-export default App;
+const ProductCard = (props) => {
+  return (
+    <div className="product-card">
+      <img src={props.item.product_image} style={{ maxWidth: '100%' }} alt=""/>
+      <span className="product-name">{props.item.product_name}</span>
+      <div className="product-description">
+        {props.item.product_detail}
+      </div>
+      <div className="product-price">
+        ฿{props.item.price}
+      </div>
+      <button className="btn btn-success btn-lg">Buy</button>
+    </div>
+  )
+}
+
+class ProductList extends React.Component {
+  state = {
+    // key: value
+    products: [
+      {
+        product_name: 'Oreo',
+        product_detail: 'Biscuit. 112g',
+        price: 270.00
+      },
+      {
+        product_name: '3D Mask ผู้ใหญ่',
+        product_detail: '3D Mask ผู้ใหญ่ บรรจุ 4 ชิ้น',
+        price: 129.00
+      },
+      {
+        product_name: 'กะเพรา',
+        product_detail: 'กะเพรา 4 ต้น',
+        price: 10.50
+      },
+      {
+        product_name: 'Bar-B-Plaza E-Vouncher',
+        product_detail: 'e-vouncher 100.-',
+        price: 85.00
+      },
+    ],
+    user: {
+      name: 'Bas'
+    }
+  }
+
+  render() {
+    return (
+      <div className="container px-0">
+        <Title name={this.state.user.name} />
+        <Title name="New Product!" />
+        <ProductCard item={this.state.products[0]} />
+        <Title name="Product list"/>
+        <div className="product-list">
+          {this.state.products.map(
+            (product) => (
+              <ProductCard key={product.id} item={product} />
+            )
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ProductList
